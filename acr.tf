a16af890b2a4f8067075963e63783c48fd04d000
danielkakazu <65232109+danielkakazu@users.noreply.github.com>
2025-09-16 10:19:09 -0300

    Update acr.tf

--- acr.tf
+++ acr.tf
@@ -1,5 +1,12 @@
+resource "random_string" "acr_suffix" {
+  length  = 6
+  upper   = false
+  number  = true
+  special = false
+}
+
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
+  name                = "airflowacr${random_string.acr_suffix.result}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Premium"
