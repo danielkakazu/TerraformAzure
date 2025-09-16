9219166d1c3e438a0671c798d0dc4c89db615a2b
danielkakazu <65232109+danielkakazu@users.noreply.github.com>
2025-09-16 11:20:14 -0300

    Add files via upload

--- aks.tf
+++ aks.tf
@@ -28,6 +28,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-output "kube_config_raw" {
-  value = azurerm_kubernetes_cluster.aks.kube_config[0].raw_kube_config
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_admin_config[0].raw_kube_config
+  sensitive = true
 }
